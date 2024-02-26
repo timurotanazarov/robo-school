33487840fde275046b4f3eda60038c0ef408a5b1
Temur <luvbrawl999@gamil.com>
2024-02-26 16:02:34 +0500

    header 50%

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Logo from '../../../public/imgs/Logo.png'
 
 function header() {
   return (
-    <div>header</div>
+    <div className="header">
+      <div className="container">
+        <div className="header-row">
+          <div className="header-logo">
+            <img src={Logo} alt="" />
+          </div>
+          <div className="header-links">
+            <a href="">О школе</a>
+            <a href="">Тренеры</a>
+            <a href="">Стоимость</a>
+            <a href="">Контакты</a>
+          </div>
+          <a href="tel:+7 800 000 11 22" className="header-tel">+7 800 000 11 22</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
